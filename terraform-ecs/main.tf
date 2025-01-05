333f2cd8fdca66ba43451352e80834ce9dbc0a4e
Lena <lena@Lenas-MBP.elecom>
2025-01-05 18:08:06 +0900

    add workflows

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -8,8 +8,27 @@ resource "aws_ecs_cluster" "main" {
 }
 
 # 创建 IAM 角色
-data "aws_iam_role" "ecs_task_execution_role" {
+resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# 为 ECS 任务角色附加权限
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # ECS 任务定义
