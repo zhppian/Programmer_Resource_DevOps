ffe79125a38fd58088547d3e8145d505e0821243
Lena <lena@Lenas-MBP.elecom>
2025-01-05 17:22:14 +0900

    add workflows

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -5,21 +5,8 @@ provider "aws" {
 resource "aws_ecs_cluster" "main" {
   name = "program-resource-tf"
 }
-resource "aws_iam_role" "ecs_task_execution_role" {
+data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs-task-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
