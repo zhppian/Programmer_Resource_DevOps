0917aeb4f9c740df3600de1f40e63d3b3388ec6e
Lena <lena@Lenas-MBP.elecom>
2025-01-05 18:40:08 +0900

    add workflows

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -2,29 +2,20 @@ provider "aws" {
   region = "ap-northeast-3"
 }
 
-# 定义 ECS Cluster
 resource "aws_ecs_cluster" "main" {
   name = "program-resource-tf"
 }
-
-# 使用既存 IAM Role
 data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs-task-execution-role"
 }
 
-# 定义 ECS Task Definition
 resource "aws_ecs_task_definition" "program_resource" {
-  family                   = "program-resource-tf"
+  family = "program-resource-tf"
   network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
-  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([
+  container_definitions    = jsonencode([
     {
       name      = "frontend-container"
-      image     = "886436941040.dkr.ecr.ap-northeast-3.amazonaws.com/program_resource_frontend:latest" # 确保镜像最新
+      image     = "886436941040.dkr.ecr.ap-northeast-3.amazonaws.com/program_resource_frontend:latest"
       cpu       = 256
       memory    = 512
       essential = true
@@ -37,7 +28,7 @@ resource "aws_ecs_task_definition" "program_resource" {
     },
     {
       name      = "backend-container"
-      image     = "886436941040.dkr.ecr.ap-northeast-3.amazonaws.com/program_resource_backend:latest" # 确保镜像最新
+      image     = "886436941040.dkr.ecr.ap-northeast-3.amazonaws.com/program_resource_backend:latest"
       cpu       = 256
       memory    = 512
       essential = true
@@ -49,21 +40,17 @@ resource "aws_ecs_task_definition" "program_resource" {
       ]
     }
   ])
+
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn  # 添加执行角色
 }
 
-# 加载默认 VPC 和子网
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# 定义安全组
 resource "aws_security_group" "http" {
   name_prefix = "http-"
   description = "Allow HTTP traffic"
@@ -75,14 +62,12 @@ resource "aws_security_group" "http" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 5001
     to_port     = 5001
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -91,27 +76,25 @@ resource "aws_security_group" "http" {
   }
 }
 
-# 定义 ECS 服务
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 resource "aws_ecs_service" "main" {
   name            = "program-resource-frontend-tf"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.program_resource.arn
-  desired_count   = 1
+  desired_count   = 0
   launch_type     = "FARGATE"
+
 
   network_configuration {
     subnets          = data.aws_subnets.default.ids
     security_groups  = [aws_security_group.http.id]
     assign_public_ip = true
   }
-
-  # 防止不必要的销毁和重建
-  lifecycle {
-    ignore_changes = [
-      desired_count, # 忽略实例数量的变化
-    ]
-  }
-
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
 }
+
