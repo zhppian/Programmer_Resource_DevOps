429c5e27f204b979ba591e422963e2b198f26712
Lena <lena@Lenas-MBP.elecom>
2025-01-05 18:14:08 +0900

    add workflows

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -19,7 +19,7 @@ resource "aws_ecs_task_definition" "program_resource" {
   requires_compatibilities = ["FARGATE"]
   cpu                   = "512"
   memory                = "1024"
-  execution_role_arn    = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn    = data.aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
