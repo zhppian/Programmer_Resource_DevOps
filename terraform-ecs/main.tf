c7d96b630ccffe9a1f74f7fd0da288a8a022e39b
Lena <lena@Lenas-MBP.elecom>
2025-01-12 19:48:52 +0900

    modify terraform

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -61,7 +61,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# 使用现有手动创建的安全组
 data "aws_security_group" "http" {
   filter {
     name   = "group-name"
@@ -76,6 +75,60 @@ data "aws_subnets" "default" {
   }
 }
 
+### Load Balancer 配置开始 ###
+# 创建 ALB
+resource "aws_lb" "main" {
+  name               = "program-resource-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [data.aws_security_group.http.id]
+  subnets            = data.aws_subnets.default.ids
+}
+
+# 创建 ALB 的目标组 (Frontend)
+resource "aws_lb_target_group" "frontend_tg" {
+  name        = "frontend-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip" # ECS 使用 IP 模式
+}
+
+# 创建 ALB 的目标组 (Backend)
+resource "aws_lb_target_group" "backend_tg" {
+  name        = "backend-target-group"
+  port        = 5001
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip" # ECS 使用 IP 模式
+}
+
+# 创建 Listener (Frontend)
+resource "aws_lb_listener" "frontend_listener" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+  # 当流量到达 frontend_listener 的 80 端口时，ALB 将其转发到 frontend-target-group 中注册的目标
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+  }
+}
+
+# 创建 Listener (Backend)
+resource "aws_lb_listener" "backend_listener" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 5001
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+  }
+}
+### Load Balancer 配置结束 ###
+
+# 更新 ECS 服务以使用 ALB
 resource "aws_ecs_service" "main" {
   name            = "program-resource-tf"
   cluster         = aws_ecs_cluster.main.id
@@ -88,4 +141,21 @@ resource "aws_ecs_service" "main" {
     security_groups  = [data.aws_security_group.http.id]
     assign_public_ip = true
   }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+    container_name   = "frontend-container"
+    container_port   = 80
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+    container_name   = "backend-container"
+    container_port   = 5001
+  }
+  # 确保 ALB 和目标组的 Listener 在 ECS 服务之前创建
+  depends_on = [
+    aws_lb_listener.frontend_listener,
+    aws_lb_listener.backend_listener
+  ]
 }
