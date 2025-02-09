f85cae6f916b86c6cf4546fcea6d5d13b46ac704
Lena <lena@Lenas-MBP.elecom>
2025-02-09 19:46:24 +0900

    modify tf

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -126,7 +126,89 @@ resource "aws_lb_listener" "backend_listener" {
     target_group_arn = aws_lb_target_group.backend_tg.arn
   }
 }
+
+# 创建 HTTPS Listener (Frontend)
+resource "aws_lb_listener" "frontend_https_listener" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"  # 可根据需要选择 SSL 策略
+  certificate_arn   = "arn:aws:acm:ap-northeast-3:886436941040:certificate/03ea08ec-55cb-49f2-81f9-5105b1b75420" # 替换为实际 ACM 证书 ARN
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+  }
+}
+
+# 创建 Listener (Frontend)
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.main.arn  # 关联的 ALB ARN
+  port              = 80  # HTTP 80 端口
+  protocol          = "HTTP"  # 协议为 HTTP
+  default_action {
+    type = "redirect"  # 重定向
+    redirect {
+      port        = "443"  # 重定向到 HTTPS 443
+      protocol    = "HTTPS"  # 使用 HTTPS 协议
+      status_code = "HTTP_301"  # 重定向状态码
+    }
+  }
+}
+
+-- resource "aws_lb_listener_rule" "backend_path_rule" {
+--   listener_arn = aws_lb_listener.frontend_https_listener.arn
+--   priority     = 10
+
+--   condition {
+--     path_pattern {
+--       values = ["/api/*"]
+--     }
+--   }
+
+--   action {
+--     type             = "forward"
+--     target_group_arn = aws_lb_target_group.backend_tg.arn
+--   }
+-- }
+
+-- resource "aws_lb_listener_rule" "frontend_path_rule" {
+--   listener_arn = aws_lb_listener.frontend_https_listener.arn
+--   priority     = 20
+
+--   condition {
+--     path_pattern {
+--       values = ["/*"]
+--     }
+--   }
+
+--   action {
+--     type             = "forward"
+--     target_group_arn = aws_lb_target_group.frontend_tg.arn
+--   }
+-- }
+
 ### Load Balancer 配置结束 ###
+
+### Route53 配置 ###
+
+# 创建托管区域
+resource "aws_route53_zone" "program_resource_hub" {
+  name = "programresourcehub.com"  # 域名
+}
+
+# 创建 A 记录并路由到 ALB
+resource "aws_route53_record" "root_record" {
+  zone_id = aws_route53_zone.program_resource_hub.zone_id  # 托管区域 ID
+  name    = "programresourcehub.com"  # 根域名记录
+  type    = "A"  # A 记录
+  alias {
+    name                   = aws_lb.main.dns_name  # ALB 的 DNS 名称
+    zone_id                = aws_lb.main.zone_id  # ALB 的 Zone ID
+    evaluate_target_health = true  # 评估目标健康状态
+  }
+}
+### Route53 配置结束 ###
 
 # 更新 ECS 服务以使用 ALB
 resource "aws_ecs_service" "main" {
